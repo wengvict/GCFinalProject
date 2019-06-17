ba12beb990f6c0d0de9590a79b846d811025c55c
Victoria Weng <wengvict@gmail.com>
2019-06-17 12:06:51 -0400

    first commit, tested and runs

---
+++ src/test/java/com/finalproject/PlantApp/PlantAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.finalproject.PlantApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PlantAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
